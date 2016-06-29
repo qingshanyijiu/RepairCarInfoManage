4861bee6e52e2ce6d0732b6e869367d4843ddd80
qingshanyijiu <542187663@qq.com>
2016-06-29 12:40:08 +0800

    把右键菜单消息映射定义好

--- RepairCarInfoManage/RepairInfoQueryDlg.cpp
+++ RepairCarInfoManage/RepairInfoQueryDlg.cpp
@@ -28,7 +28,78 @@ void CRepairInfoQueryDlg::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(CRepairInfoQueryDlg, CDialogEx)
+	ON_NOTIFY(NM_RCLICK, IDC_LIST_RepairList, &CRepairInfoQueryDlg::OnRclickListRepairlist)
+	ON_NOTIFY(NM_DBLCLK, IDC_LIST_RepairList, &CRepairInfoQueryDlg::OnDblclkListRepairlist)
+	ON_COMMAND(ID_SMENU_RepairDelete, &CRepairInfoQueryDlg::OnSmenuRepairdelete)
+	ON_COMMAND(ID_SMENU_RepairDetail, &CRepairInfoQueryDlg::OnSmenuRepairdetail)
+	ON_COMMAND(ID_SMENU_RepairModify, &CRepairInfoQueryDlg::OnSmenuRepairmodify)
+	ON_COMMAND(ID_SMENU_RepairQueryUser, &CRepairInfoQueryDlg::OnSmenuRepairqueryuser)
 END_MESSAGE_MAP()
 
 
 // CRepairInfoQueryDlg message handlers
+
+
+void CRepairInfoQueryDlg::OnRclickListRepairlist(NMHDR *pNMHDR, LRESULT *pResult)
+{
+	LPNMITEMACTIVATE pNMItemActivate = reinterpret_cast<LPNMITEMACTIVATE>(pNMHDR);
+	// TODO: Add your control notification handler code here
+	NM_LISTVIEW* pNMListView = (NM_LISTVIEW*)pNMHDR;
+	//if(pNMListView->iItem != -1)
+	{
+		DWORD dwPos = GetMessagePos();
+		CPoint point( LOWORD(dwPos), HIWORD(dwPos) );
+
+		CMenu menu;
+		VERIFY( menu.LoadMenu( IDR_MENU_RepairMenu ) );
+		CMenu* popup = menu.GetSubMenu(0);
+		ASSERT( popup != NULL );
+		popup->TrackPopupMenu(TPM_LEFTALIGN | TPM_RIGHTBUTTON, point.x, point.y, this );
+	}
+	*pResult = 0;
+}
+
+
+void CRepairInfoQueryDlg::OnDblclkListRepairlist(NMHDR *pNMHDR, LRESULT *pResult)
+{
+	LPNMITEMACTIVATE pNMItemActivate = reinterpret_cast<LPNMITEMACTIVATE>(pNMHDR);
+	// TODO: Add your control notification handler code here
+	
+
+	*pResult = 0;
+}
+
+
+void CRepairInfoQueryDlg::OnSmenuRepairdelete()
+{
+	// TODO: Add your command handler code here
+}
+
+
+void CRepairInfoQueryDlg::OnSmenuRepairdetail()
+{
+	// TODO: Add your command handler code here
+}
+
+
+void CRepairInfoQueryDlg::OnSmenuRepairmodify()
+{
+	// TODO: Add your command handler code here
+}
+
+
+void CRepairInfoQueryDlg::OnSmenuRepairqueryuser()
+{
+	// TODO: Add your command handler code here
+}
+
+
+BOOL CRepairInfoQueryDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  Add extra initialization here
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
--- RepairCarInfoManage/RepairInfoQueryDlg.h
+++ RepairCarInfoManage/RepairInfoQueryDlg.h
@@ -18,4 +18,12 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnRclickListRepairlist(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnDblclkListRepairlist(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnSmenuRepairdelete();
+	afx_msg void OnSmenuRepairdetail();
+	afx_msg void OnSmenuRepairmodify();
+	afx_msg void OnSmenuRepairqueryuser();
+	virtual BOOL OnInitDialog();
 };
